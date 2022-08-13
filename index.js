077d04a542c5aab60f4a4fea45950d22f6f3574c
kakwizard <80742169+kakwizard@users.noreply.github.com>
2022-08-13 14:48:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Client({ intents: GatewayIntentBits.Guilds, })
 
 client.on("ready", async () => {
   client.user.setPresence({ status: "dnd"}) // Type Status dnd , idle , online
-  client.user.setActivity({ name: `By SmSm`, type: ActivityType.Playing }) // Type Activity Playing , Watching , Streaming 
+  client.user.setActivity({ name: `BAWKA GAWRAY PROJECT KAK WIZARD KT UP`, type: ActivityType.Playing }) // Type Activity Playing , Watching , Streaming 
   console.log(client.user.tag)
 })
 
@@ -28,5 +28,5 @@ console.log(err)
 });
 
 
-client.login("TOKENBOT")
+client.login("OTkxNDI1OTMyMTY4NTM2MTU1.Goqlql.AtB9zpHv1MH5O3HBWl1WC8enUhJHp2AA1hN5aU")
 // All Copyright By SmSm
